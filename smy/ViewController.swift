91999f6b58d9717836994b56e39c2fa5c91f63e5
Damon1211 <758568716@qq.com>
2017-04-26 14:12:21 +0800

    清理冗余代码

--- smy/ViewController.swift
+++ smy/ViewController.swift
@@ -16,59 +16,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //1.设置访问资源
-//        let urlData = URL(string: "http://res.anxinlirong.com/static/appurl.txt");
-//        
-//        print(urlData?.absoluteString ?? "url is none")
-//        //2.建立网络请求
-//        let requestUrl = URLRequest(url: urlData!);
-//        
-//        //3.发送请求
-//        NSURLConnection.sendAsynchronousRequest(requestUrl, queue:OperationQueue()) { (res, data, error)in
-//            //服务器返回：请求方式 = GET，返回数据格式 = JSON，用户名 = 123，密码 = 1233
-//            let urlPrefix = NSString(data: data!, encoding:String.Encoding.utf8.rawValue)! as String;
-//        
-//            // Do any additional setup after loading the view, typically from a nib.
-//            // 1.设置访问资源 -
-//            let param = "{'deviceId':'"+CloudPushSDK.getDeviceId()+"'}";
-//            
-//            let encodeParam = param.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed);
-//            
-//            let url = URL(string: urlPrefix + "/?param="+encodeParam!);
-//            
-//            //print("=============="+(url?.absoluteString)!)
-//            
-//            //print(url?.absoluteString ?? "url is none")
-//            // 2.建立网络请求
-//            let request = URLRequest(url: url!);
-//            // 3.加载网络请求
-//            self.webView.loadRequest(request);
-//        }
-//        
         
         //方案
+        //1.设置访问资源
         let Url = URL.init(string: "http://res.anxinlirong.com/static/appurl.txt")
         
         let request = NSMutableURLRequest.init(url: Url!)
         
         request.timeoutInterval = 30
-        //请求方式，跟OC一样的
+        
+        //2.请求方式，跟OC一样的
         request.httpMethod = "GET"
-        request.httpBody = "".data(using: String.Encoding.utf8)
         
         let session = URLSession.shared
         let dataTask = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
             if (error != nil) {
                 return
             }else {
-                
                 var responseStr = String(data: data!,encoding:String.Encoding.utf8);
                 
-                
-                
-                
                 responseStr = responseStr?.replacingOccurrences(of: "\n", with: "");
-                print(responseStr)
+                
                 // Do any additional setup after loading the view, typically from a nib.
                 // 1.设置访问资源 -
 //                let param = "{'deviceId':'"+CloudPushSDK.getDeviceId()+"'}";
@@ -77,7 +45,6 @@ class ViewController: UIViewController {
                 let deviceId = CloudPushSDK.getDeviceId();
                 
                 let param = "{'deviceId':'"+deviceId!+"'}"
-                
                 
                 let encodeParam = param.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed);
 //
